9ba7d056722723558d8a647784e941122f2df8cb
CaL616 <cjoneslarson@gmail.com>
2026-06-09 19:01:12 -0700

    Update contact.js

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -9,19 +9,14 @@
         var isFormValid = true;
 
         for (var i = 0; i < elements.length; i++) {
-
             var el = elements[i];
-
             if (el.type === 'submit' || el.type === 'button') continue;
-
-            var isValid = validateRequired(el);
-
+                var isValid = validateRequired(el);
             if (!isValid) {
                 showErrorMessage(el);
             } else {
                 removeErrorMessage(el);
             }
-
             if (el.id) {
                 valid[el.id] = isValid;
             }
@@ -31,36 +26,27 @@
             showErrorMessage(document.getElementById('email'));
             valid.email = false;
         }
-
         for (var field in valid) {
             if (!valid[field]) {
                 isFormValid = false;
                 break;
             }
         }
-
         if (!isFormValid) {
             e.preventDefault();
         }
-
     });
 
     function validateRequired(el) {
-
         if (el.hasAttribute('required')) {
-
             var valid = !isEmpty(el);
-
             if (!valid) {
                 setErrorMessage(el, 'This field is required');
             }
-
             return valid;
         }
-
         return true;
     }
-
     function isEmpty(el) {
         return !el.value;
     }
@@ -78,26 +64,20 @@
 
         return valid;
     }
-
     function setErrorMessage(el, message) {
         $(el).data('errorMessage', message);
     }
-
     function showErrorMessage(el) {
-
         var $el = $(el);
         var $errorContainer = $el.siblings('.text-danger');
-
         if (!$errorContainer.length) {
             $errorContainer = $('<span class="text-danger small"></span>')
                 .insertAfter($el);
         }
-
         $errorContainer.text($el.data('errorMessage'));
     }
 
     function removeErrorMessage(el) {
         $(el).siblings('.text-danger').text('');
     }
-
 })();
